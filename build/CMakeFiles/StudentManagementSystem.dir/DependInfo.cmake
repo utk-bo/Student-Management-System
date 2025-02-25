6e0f36405c462e36d6aebb6219e8a53aa94f6988
jolanallen <jolanallen34@gmail.com>
2025-02-25 19:57:37 +0100

    classe student finished

---
+++ build/CMakeFiles/StudentManagementSystem.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jolan/Student-Management-System/src/API.cpp" "CMakeFiles/StudentManagementSystem.dir/src/API.cpp.o" "gcc" "CMakeFiles/StudentManagementSystem.dir/src/API.cpp.o.d"
+  "/home/jolan/Student-Management-System/src/Course.cpp" "CMakeFiles/StudentManagementSystem.dir/src/Course.cpp.o" "gcc" "CMakeFiles/StudentManagementSystem.dir/src/Course.cpp.o.d"
+  "/home/jolan/Student-Management-System/src/Enrollment.cpp" "CMakeFiles/StudentManagementSystem.dir/src/Enrollment.cpp.o" "gcc" "CMakeFiles/StudentManagementSystem.dir/src/Enrollment.cpp.o.d"
+  "/home/jolan/Student-Management-System/src/GraduateStudent.cpp" "CMakeFiles/StudentManagementSystem.dir/src/GraduateStudent.cpp.o" "gcc" "CMakeFiles/StudentManagementSystem.dir/src/GraduateStudent.cpp.o.d"
+  "/home/jolan/Student-Management-System/src/Person.cpp" "CMakeFiles/StudentManagementSystem.dir/src/Person.cpp.o" "gcc" "CMakeFiles/StudentManagementSystem.dir/src/Person.cpp.o.d"
+  "/home/jolan/Student-Management-System/src/Professor.cpp" "CMakeFiles/StudentManagementSystem.dir/src/Professor.cpp.o" "gcc" "CMakeFiles/StudentManagementSystem.dir/src/Professor.cpp.o.d"
+  "/home/jolan/Student-Management-System/src/Student.cpp" "CMakeFiles/StudentManagementSystem.dir/src/Student.cpp.o" "gcc" "CMakeFiles/StudentManagementSystem.dir/src/Student.cpp.o.d"
+  "/home/jolan/Student-Management-System/src/UndergaduateStudent.cpp" "CMakeFiles/StudentManagementSystem.dir/src/UndergaduateStudent.cpp.o" "gcc" "CMakeFiles/StudentManagementSystem.dir/src/UndergaduateStudent.cpp.o.d"
+  "/home/jolan/Student-Management-System/src/main.cpp" "CMakeFiles/StudentManagementSystem.dir/src/main.cpp.o" "gcc" "CMakeFiles/StudentManagementSystem.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/StudentManagementSystem.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StudentManagementSystem.dir/src/API.cpp.o"
+  "CMakeFiles/StudentManagementSystem.dir/src/API.cpp.o.d"
+  "CMakeFiles/StudentManagementSystem.dir/src/Course.cpp.o"
+  "CMakeFiles/StudentManagementSystem.dir/src/Course.cpp.o.d"
+  "CMakeFiles/StudentManagementSystem.dir/src/Enrollment.cpp.o"
+  "CMakeFiles/StudentManagementSystem.dir/src/Enrollment.cpp.o.d"
+  "CMakeFiles/StudentManagementSystem.dir/src/GraduateStudent.cpp.o"
+  "CMakeFiles/StudentManagementSystem.dir/src/GraduateStudent.cpp.o.d"
+  "CMakeFiles/StudentManagementSystem.dir/src/Person.cpp.o"
+  "CMakeFiles/StudentManagementSystem.dir/src/Person.cpp.o.d"
+  "CMakeFiles/StudentManagementSystem.dir/src/Professor.cpp.o"
+  "CMakeFiles/StudentManagementSystem.dir/src/Professor.cpp.o.d"
+  "CMakeFiles/StudentManagementSystem.dir/src/Student.cpp.o"
+  "CMakeFiles/StudentManagementSystem.dir/src/Student.cpp.o.d"
+  "CMakeFiles/StudentManagementSystem.dir/src/UndergaduateStudent.cpp.o"
+  "CMakeFiles/StudentManagementSystem.dir/src/UndergaduateStudent.cpp.o.d"
+  "CMakeFiles/StudentManagementSystem.dir/src/main.cpp.o"
+  "CMakeFiles/StudentManagementSystem.dir/src/main.cpp.o.d"
+  "StudentManagementSystem"
+  "StudentManagementSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StudentManagementSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
